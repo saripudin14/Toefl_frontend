54dea8b926f4ad3a346bb1a388cb3dd77f9974b8
saripudin14 <m.arifrivaldi141105@gmail.com>
2026-06-29 15:55:34 +0700

    chore: update base path and configure path aliases in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import path from "path";
+import { fileURLToPath } from "url";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
+// Tentukan __dirname secara manual untuk ESM
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/Toefl_frontend/',
-})
+  base: "/Toefl_PrepPro/", // <-- INI YANG PALING PENTING
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
